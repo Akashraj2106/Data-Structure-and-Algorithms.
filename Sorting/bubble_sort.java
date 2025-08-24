e4dd14d2fb42464395e675d68151a529e3f3fe6f
Akashraj2106 <23cs115akas@ug.cusat.ac.in>
2025-08-24 10:26:28 +0530

    first commit

---
+++ Sorting/bubble_sort.java
@@ -0,0 +1,33 @@
+
+public class bubble_sort {
+
+    public static void swap(int arr[], int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+
+    }
+
+    public static void bubble_sorts(int arr[]) {
+        for (int term = 0; term < arr.length - 1; term++) {  // from 0 to 3
+            for (int j = 0; j < arr.length - 1 - term; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+    }
+
+    public static void print(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5, 4, 1, 3, 2};
+
+        bubble_sorts(arr);
+        print(arr);
+    }
+}
